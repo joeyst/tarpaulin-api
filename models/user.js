d0646e5c918e15788d73bde66eb32a645e670c0a
joeyst <joeystenbeck@gmail.com>
2024-06-05 15:03:23 -0700

    Add lib/bcrypt.js, lib/jsonwebtoken.js.

--- models/user.js
+++ models/user.js
@@ -10,11 +10,14 @@ const UserSchema = {
 }
 
 async function getUserInfoById(id) {
+  // TODO: Add check for id = requested user ID. 
   if (!ObjectId.isValid(id)) { return null }
   return getMongoCollection('users').findOne({ _id: new ObjectId(id) })
 }
 
 async function addUser(userObject): string {
+  // TODO: Check if userObject.id == decoded user ID. 
+  // TODO: Salt userObject.password with bcrypt's hashSync (mutate userObject). 
   return getMongoCollection('users')
     .insertOne(userObject)
     .then(result => result.insertedId) // TODO: result.insertedId.toString()? 
