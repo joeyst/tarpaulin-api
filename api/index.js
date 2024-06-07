61cef6391f0426de2669bf85ccb1f732674c641b
joeyst <joeystenbeck@gmail.com>
2024-06-07 11:39:12 -0700

    Finish appendJwtLoginInfo.

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const { Router } = require('express')
 
 const router = Router()
 
+router.use()
+
 router.use('/users', require('./users'))
 router.use('/courses', require('./courses'))
 router.use('/assignments', require('./assignments'))
